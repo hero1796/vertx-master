5866ed740383b95d54d0d252558e89b952e14179
hungvt <vuthehung17@gmail.com>
2019-09-20 11:42:38 +0700

    first

---
+++ api/src/main/java/User.java
@@ -0,0 +1,31 @@
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonAutoDetect
+class User {
+
+  // Variables
+
+  private String mFirstName;
+  private String mLastName;
+
+  // Constructors
+
+  User(String firstName, String lastName) {
+    mFirstName = firstName;
+    mLastName = lastName;
+  }
+
+  // Accessors
+
+  @JsonProperty
+  String getFirstName() {
+    return mFirstName;
+  }
+
+  @JsonProperty
+  String getLastName() {
+    return mLastName;
+  }
+}
+
